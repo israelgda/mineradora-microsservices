6d7e1046bddfda2059e26c31b37abc30c188fef0
Israel Gadelha <50384389+israelgda@users.noreply.github.com>
2024-06-03 23:26:34 -0300

    feat: Implementa ResourceNotFoundException e Mapper

---
+++ ms-report/src/main/kotlin/org/israelgda/services/exceptions/ResourceNotFoundMapper.kt
@@ -0,0 +1,15 @@
+package org.israelgda.services.exceptions
+
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.ext.ExceptionMapper
+import jakarta.ws.rs.ext.Provider
+
+@Provider
+class ResourceNotFoundMapper: ExceptionMapper<ResourceNotFoundException> {
+    override fun toResponse(exception: ResourceNotFoundException): Response {
+        return Response
+            .status(Response.Status.NOT_FOUND)
+            .entity(ExceptionSimpleMessage(exception.message))
+            .build()
+    }
+}
