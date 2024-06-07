c0ef5e986599fc10a4bd05d2a07292830c99197e
Israel Gadelha <50384389+israelgda@users.noreply.github.com>
2024-06-06 23:24:54 -0300

    feat: Adiciona implementação da autenticação com Keycloak

--- ms-proposal/src/main/kotlin/org/israelgda/controllers/ProposalController.kt
+++ ms-proposal/src/main/kotlin/org/israelgda/controllers/ProposalController.kt
@@ -1,22 +1,30 @@
 package org.israelgda.controllers
 
+import io.quarkus.security.Authenticated
+import jakarta.annotation.security.RolesAllowed
 import jakarta.inject.Inject
 import jakarta.transaction.Transactional
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
+import org.eclipse.microprofile.jwt.JsonWebToken
 import org.israelgda.dto.ProposalDetailsDTO
 import org.israelgda.services.ProposalService
 
 @Path("/api/proposal")
+@Authenticated
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 class ProposalController {
 
     @Inject
+    lateinit var jwt: JsonWebToken
+
+    @Inject
     lateinit var proposalService: ProposalService
 
     @GET
+    @RolesAllowed("manager", "user")
     @Path("/{id}")
     fun findProposalDetailed(@PathParam("id") id: Long): Response {
         return Response.ok()
@@ -25,6 +33,7 @@ class ProposalController {
     }
 
     @POST
+    @RolesAllowed("proposal-customer")
     @Transactional
     fun create(proposalDetailsDTO: ProposalDetailsDTO): Response {
         proposalService.create(proposalDetailsDTO)
@@ -33,6 +42,7 @@ class ProposalController {
     }
 
     @DELETE
+    @RolesAllowed("manager")
     @Path("/{id}")
     @Transactional
     fun delete(@PathParam("id") id: Long): Response {
